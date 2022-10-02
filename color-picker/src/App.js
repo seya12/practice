6514f07de704d285a013549a4847ad9e350fa120
seya12 <lukas.seyr@gmx.at>
2022-10-02 14:22:57 +0200

    added color box, some styling and a json with hex colors

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import colors from "./res/colors.json";
 
 function App() {
-  return <h1>Hello</h1>;
+  const [color, setColor] = useState("");
+  const [guess, setGuess] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  const checkGuess = (e) => {
+    setClicked(true);
+    const correct = e.target.textContent === color;
+    setGuess(correct);
+    if (correct) {
+      setColor(colors[1].hex);
+    }
+  };
+
+  useEffect(() => {
+    setColor(colors[0].hex);
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="color-box" style={{ backgroundColor: color }}></div>
+      <div className="guesses">
+        <button className="guess-button" onClick={checkGuess}>
+          {colors[0].hex}
+        </button>
+        <button className="guess-button" onClick={checkGuess}>
+          {colors[1].hex}
+        </button>
+        <button className="guess-button" onClick={checkGuess}>
+          {colors[2].hex}
+        </button>
+      </div>
+      {clicked && (guess ? <p>Success</p> : <p>Error</p>)}
+    </div>
+  );
 }
 
 export default App;
