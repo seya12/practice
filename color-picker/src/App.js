068a642f38dbbc37049fdb10574efcea6163ddd7
seya12 <lukas.seyr@gmx.at>
2022-10-03 12:51:04 +0200

    finished app

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -1,40 +1,60 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import colors from "./res/colors.json";
 
 function App() {
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState({ name: "", hex: "" });
   const [guess, setGuess] = useState(false);
   const [clicked, setClicked] = useState(false);
+  const [guessOptions, setGuessOptions] = useState([{ name: "", hex: "" }]);
+
+  const getRandomNumber = useCallback(() => {
+    return Math.floor(Math.random() * colors.length);
+  }, []);
+
+  const fillOptions = useCallback(() => {
+    const options = [];
+    const randomColor = getRandomNumber();
+    setColor(colors[randomColor]);
+
+    options[0] = colors[randomColor];
+    options[1] = colors[getRandomNumber()];
+    options[2] = colors[getRandomNumber()];
+
+    setGuessOptions([...options].sort(() => Math.random() - 0.5));
+  }, [getRandomNumber]);
+
+  useEffect(() => {
+    fillOptions();
+  }, [fillOptions]);
 
   const checkGuess = (e) => {
     setClicked(true);
-    const correct = e.target.textContent === color;
+    const correct = e.target.textContent === color.hex;
     setGuess(correct);
     if (correct) {
-      setColor(colors[1].hex);
+      fillOptions();
     }
   };
 
-  useEffect(() => {
-    setColor(colors[0].hex);
-  }, []);
-
   return (
     <div className="app">
-      <div className="color-box" style={{ backgroundColor: color }}></div>
+      <div className="color-box" style={{ backgroundColor: color.hex }}></div>
       <div className="guesses">
-        <button className="guess-button" onClick={checkGuess}>
-          {colors[0].hex}
-        </button>
-        <button className="guess-button" onClick={checkGuess}>
-          {colors[1].hex}
-        </button>
-        <button className="guess-button" onClick={checkGuess}>
-          {colors[2].hex}
-        </button>
+        {guessOptions.map(({ hex }) => {
+          return (
+            <button key={hex} className="guess-button" onClick={checkGuess}>
+              {hex}
+            </button>
+          );
+        })}
       </div>
-      {clicked && (guess ? <p>Success</p> : <p>Error</p>)}
+      {clicked &&
+        (guess ? (
+          <p className="success">Success!</p>
+        ) : (
+          <p className="error">Wrong Answer</p>
+        ))}
     </div>
   );
 }
